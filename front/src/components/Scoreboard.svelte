f712a18c8ff1becc648d1bd2bb8792d953dd128f
choozn <65042105+choozn@users.noreply.github.com>
2023-11-25 04:17:02 +0100

    front: add Scoreboard component

---
+++ front/src/components/Scoreboard.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import type { Team } from "../interfaces/index";
+  export let teams: Team[];
+</script>
+
+<div class="main">
+  {#each teams as team}
+    <div class="team">
+      <div class="name">
+        {team.id}
+      </div>
+      <div class="score">
+        {team.score}
+      </div>
+    </div>
+  {/each}
+</div>
+
+<style>
+  .main {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
